367cb4922ea96f40c302dc9fa98d5987613c4dcc
Niklas Gerdt <gerdtni@gmail.com>
2014-08-26 22:21:36 +0300

    more tests

--- scala/src/test/scala/simulatorTest.scala
+++ scala/src/test/scala/simulatorTest.scala
@@ -12,13 +12,13 @@ class SimulatorTest extends BaseTest {
     val c = Config(1, 1, 1)
     val s = new Simulator with Mock
     s run List(c)
-    s.invocations should be(1)
+    s.events.size should be(1)
   }
 
   it should "send notifications if no confs" in {
     val s = new Simulator with Mock
     s run List()
-    s.invocations should be(0)
+    s.events.size should be(0)
   }
 
   it should "send notifications as conffed value" in {
@@ -26,13 +26,22 @@ class SimulatorTest extends BaseTest {
     val c1 = Config(3, 1, 1)
     val c2 = Config(2, 1, 1)
     s run List(c1, c2)
-    s.invocations should be(3 + 2)
+    s.events.size should be(3 + 2)
   }
 
-  trait Mock extends Pub {
-    var invocations = 0
+it should "send notification as conffed length" in {
+   val s = new Simulator with Mock
+    val c1 = Config(3, 5, 1)
+    val c2 = Config(2, 2, 1)
+    s run List(c1, c2)
+val r = s.events.filter(_.msg.length == 5) 
+  r.size should be(3)
+ }
 
-    override def pub(n: Notification) = { invocations = invocations + 1 }
+  trait Mock extends Pub {
+    var events: List[Notification] = List()
+
+    override def pub(n: Notification) = { events = events :+ n }
   }
 }
 
