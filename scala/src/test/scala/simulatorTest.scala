7044731a1c1129049fa65cd2f11d2b5bc3ad1694
Niklas Gerdt <gerdtni@gmail.com>
2014-08-26 19:03:57 +0300

    more simulator tests

--- scala/src/test/scala/simulatorTest.scala
+++ scala/src/test/scala/simulatorTest.scala
@@ -7,16 +7,31 @@ import org.scalatest._
 import eu.route20.hft.simulator._
 
 class SimulatorTest extends BaseTest {
-  var invocations = 0
 
   "Simulator" should "publish notifications" in {
     val c = Config(1, 1, 1)
     val s = new Simulator with Mock
     s run List(c)
-    invocations should be(1)
+    s.invocations should be(1)
+  }
+
+  it should "send notifications if no confs" in {
+    val s = new Simulator with Mock
+    s run List()
+    s.invocations should be(0)
+  }
+
+  it should "send notifications as conffed value" in {
+    val s = new Simulator with Mock
+    val c1 = Config(3, 1, 1)
+    val c2 = Config(2, 1, 1)
+    s run List(c1, c2)
+    s.invocations should be(3 + 2)
   }
 
   trait Mock extends Pub {
+    var invocations = 0
+
     override def pub(n: Notification) = { invocations = invocations + 1 }
   }
 }
