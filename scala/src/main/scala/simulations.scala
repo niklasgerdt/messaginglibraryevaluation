e785455830307aa9dfb9184697e5df6f5f8a8cfd
Niklas Gerdt <gerdtni@gmail.com>
2014-09-01 21:39:47 +0300

    dead code

--- scala/src/main/scala/simulations.scala
+++ scala/src/main/scala/simulations.scala
@@ -6,9 +6,7 @@ import eu.route20.hft.eventsource.{Runner, SimulatorConfig, SimulatorMapper}
 import eu.route20.hft.pub._
 import grizzled.slf4j.Logging
 
-import scala.annotation.tailrec
-
-object StreamSimulation extends App with Logging with ConfValues with DummyPub with NeverKill with SimulatorMapper {
+object StreamSimulation extends App with Logging with ConfValues with DummyPub with NeverKill with SimulatorMapper with Runner {
   info("Run simulations with different configurations")
   val c1 = SimulatorConfig(tenmillion / 10, hundred, pauseForMillionMsgsPerSec)
   val c2 = SimulatorConfig(tenthousand, tenthousand, pauseForTenThousandMsgsPerSec)
@@ -18,12 +16,4 @@ object StreamSimulation extends App with Logging with ConfValues with DummyPub w
   info("running " + es.size + " simulators")
   es.par.foreach(run(_, pub))
   info("simulations done")
-
-  @tailrec def run(es: () => Option[Notification], pub: Notification => Unit): Unit = {
-    val n = es()
-    if (n.isDefined) {
-      pub(n.get)
-      run(es, pub)
-    }
-  }
 }
