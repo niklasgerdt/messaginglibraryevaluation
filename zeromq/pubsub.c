b5493138e43c9b65d5345b329d35be89e1def73d
Niklas Gerdt <niklas@debian>
2014-09-27 10:36:04 +0100

    zeromq pub-sub, termsig

--- zeromq/pubsub.c
+++ zeromq/pubsub.c
@@ -1,24 +1,49 @@
 #include <zmq.h>
 #include <assert.h>
+#include <signal.h>
+#include <stdio.h>
+
+int termsig = 0;
+void term(int signum)
+{
+    printf("Received SIGTERM, exiting...\n");
+    termsig = 1;
+}
 
 int main(){
+    printf("Setting up ZeroMQ pubsub-system\n");
+    struct sigaction action;
+    action.sa_handler = term;
+    sigaction(SIGTERM, &action, NULL);
+
     int rc;
     void *context = zmq_ctx_new();
     void *subscriber = zmq_socket(context, ZMQ_SUB);
-    rc = zmq_connect(subscriber, "tcp://localhost:5500");
+    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5501");
+    assert (rc == 0);
+    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5502");
+    assert (rc == 0);
+    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5503");
+    assert (rc == 0);
+    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5504");
+    assert (rc == 0);
+    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5505");
+    assert (rc == 0);
+    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5506");
     assert (rc == 0);
     rc = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
     assert (rc == 0);
-
     void *publisher = zmq_socket (context, ZMQ_PUB);
-    rc = zmq_bind (publisher, "tcp://*:5600");
+    rc = zmq_bind(publisher, "tcp://168.1.1.1:5600");
     assert (rc == 0);
-
+    
+    printf("ZeroMQ up\n");
     char buffer[100];
-    while (1){
+    while (termsig == 0){
         zmq_recv(subscriber, buffer, 100, 0);
         zmq_send(publisher, buffer, 100, 0);
     }
+    printf("ZeroMQ down\n");
 
     zmq_close (subscriber);
     zmq_close (publisher);
--- zeromq/sub.c
+++ zeromq/sub.c
@@ -1,7 +1,6 @@
 #include <zmq.h>
 #include <assert.h>
 #include <signal.h>
-#include <stdio.h>
 
 int termsig = 0;
 void term(int signum)
@@ -10,28 +9,27 @@ void term(int signum)
     termsig = 1;
 }
 
-void pause(long nanos){
+//char events[100][10000];
 
-}
-
-int main(int argc, char *argv[]){
+int main(){
     struct sigaction action;
     action.sa_handler = term;
     sigaction(SIGTERM, &action, NULL);
- 
+
     int rc;
     void *context = zmq_ctx_new();
-    void *publisher = zmq_socket (context, ZMQ_PUB);
-    rc = zmq_bind (publisher, "tcp://*:5500");
+    void *subscriber = zmq_socket(context, ZMQ_SUB);
+    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5601");
+    assert (rc == 0);
+    rc = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
     assert (rc == 0);
 
     char buffer[100];
     while (termsig == 0){
-        zmq_send(publisher, "test-message", 12, 0);
+        zmq_recv(subscriber, buffer, 100, 0);
     }
 
-
-    zmq_close (publisher);
+    zmq_close (subscriber);
     zmq_ctx_destroy (context);
     return 0;
 }
