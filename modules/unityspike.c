dfea6f4c30ae2e6e6a39a88f9016393bc04f63b3
Niklas Gerdt <niklas@debian>
2014-10-01 08:39:46 +0100

    unity testing framework

---
+++ modules/unityspike.c
@@ -0,0 +1,41 @@
+#include "../unity/src/unity.h"
+#include "../unity/src/unity_internals.h"
+
+#include <stdio.h>
+
+extern void test_pointerSpike(void);
+
+void test_pointerSpike(void) {
+	int *p;
+	int i = 5;
+	p = &i;
+	TEST_ASSERT_EQUAL(&i, p); //adresses match
+	TEST_ASSERT_EQUAL(i, *p); // values match
+}
+
+struct ds {
+	int i;
+};
+
+void test_struct(void) {
+	struct ds d = { .i = 100 };
+	struct ds *c = &d;
+	TEST_ASSERT_EQUAL(&d, c); // same addresses
+	TEST_ASSERT_EQUAL(d.i, c->i); // same values
+	TEST_ASSERT_EQUAL(&d.i, &c->i); // same addresses
+}
+
+static void runTest(UnityTestFunction test) {
+	test();
+}
+
+int main(int argc, char * argv[]) {
+	UnityBegin("unityspike.c");
+
+	// RUN_TEST calls runTest
+	RUN_TEST(test_pointerSpike);
+	RUN_TEST(test_struct);
+
+	UnityEnd();
+	return 0;
+}
--- zeromq/pubsub.c
+++ zeromq/pubsub.c
@@ -4,49 +4,49 @@
 #include <stdio.h>
 
 int killSignal = 0;
-void term(int signum)
-{
-    printf("Received SIGTERM, exiting...\n");
-    killSignal = 1;
+void term(int signum) {
+	printf("Received SIGTERM, exiting...\n");
+	killSignal = 1;
 }
 
-int main(){
-    struct sigaction action;
-    action.sa_handler = term;
-    sigaction(SIGTERM, &action, NULL);
+int main() {
+	struct sigaction action;
+	action.sa_handler = term;
+	sigaction(SIGTERM, &action, NULL);
 
-    printf("Setting up ZeroMQ pubsub-system\n");
-    int rc;
-    void *context = zmq_ctx_new();
-    void *subscriber = zmq_socket(context, ZMQ_SUB);
-    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5501");
-    assert (rc == 0);
-    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5502");
-    assert (rc == 0);
-    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5503");
-    assert (rc == 0);
-    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5504");
-    assert (rc == 0);
-    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5505");
-    assert (rc == 0);
-    rc = zmq_connect(subscriber, "tcp://168.1.1.2:5506");
-    assert (rc == 0);
-    rc = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
-    assert (rc == 0);
-    void *publisher = zmq_socket (context, ZMQ_PUB);
-    rc = zmq_bind(publisher, "tcp://168.1.1.1:5601");
-    assert (rc == 0);
-    
-    printf("ZeroMQ up\n");
-    char buffer[100];
-    while (killSignal == 0){
-        zmq_recv(subscriber, buffer, 100, 0);
-        zmq_send(publisher, buffer, 100, 0);
-    }
-    printf("ZeroMQ down\n");
+	printf("Setting up ZeroMQ pubsub-system\n");
+	int rc;
+	void *context = zmq_ctx_new();
+	void *subscriber = zmq_socket(context, ZMQ_SUB);
+	rc = zmq_connect(subscriber, "tcp://168.1.1.2:5501");
+	assert(rc == 0);
+	rc = zmq_connect(subscriber, "tcp://168.1.1.2:5502");
+	assert(rc == 0);
+	rc = zmq_connect(subscriber, "tcp://168.1.1.2:5503");
+	assert(rc == 0);
+	rc = zmq_connect(subscriber, "tcp://168.1.1.2:5504");
+	assert(rc == 0);
+	rc = zmq_connect(subscriber, "tcp://168.1.1.2:5505");
+	assert(rc == 0);
+	rc = zmq_connect(subscriber, "tcp://168.1.1.2:5506");
+	assert(rc == 0);
+	rc = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
+	assert(rc == 0);
+	void *publisher = zmq_socket(context, ZMQ_PUB);
+	rc = zmq_bind(publisher, "tcp://168.1.1.1:5601");
+	assert(rc == 0);
 
-    zmq_close (subscriber);
-    zmq_close (publisher);
-    zmq_ctx_destroy (context);
-    return 0;
+	printf("ZeroMQ up\n");
+	char buffer[100];
+	while (killSignal == 0) {
+		zmq_recv(subscriber, buffer, 100, 0);
+		zmq_send(publisher, buffer, 100, 0);
+	}
+	printf("ZeroMQ down\n");
+
+	zmq_close(subscriber);
+	zmq_close(publisher);
+	zmq_ctx_destroy(context);
+	return 0;
 }
+
