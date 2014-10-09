307b17f6a39245eda8821ba3621c06587bd3d791
Niklas Gerdt <gerdtni@gmail.com>
2014-10-09 12:50:07 +0100

    zmq-messages

--- src/main/c/mom/zeromqpubsub.c
+++ src/main/c/mom/zeromqpubsub.c
@@ -1,6 +1,7 @@
 #include <zmq.h>
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include "../mod/event.h"
 #include "../pubsub.h"
 
@@ -35,14 +36,14 @@ void initSub(const char *addr, const char *channel) {
 	printf("Sub connected to %s\n", addr);
 }
 
-void addSub(const char *addr){
+void addSub(const char *addr) {
 	assert(zmq_connect(subscriber, addr) == 0);
 	printf("Sub connected to %s\n", addr);
 }
 
 void destroySub() {
 	zmq_close(subscriber);
-	if (context != NULL){
+	if (context != NULL) {
 		zmq_ctx_destroy(context);
 	}
 	printf("ZeroMQ down\n");
@@ -50,14 +51,31 @@ void destroySub() {
 
 void pub(struct event e, size_t size) {
 //TODO ADD CHANNEL AS PART
-//	printf("%c;%c;%d;%lld.%09ld;%lld.%09ld;%lld.%09ld\n", e->header.source, e->header.destination, e->header.id,
-//			e->header.created.tv_sec, e->header.created.tv_nsec, e->header.published.tv_sec,
-//			e->header.published.tv_nsec, e->header.routed.tv_sec, e->header.routed.tv_nsec);
+	printf("%d\n", &e);
+	printf("%c;%c;%d;%lld.%09ld;%lld.%09ld;%lld.%09ld\n", e.header.source, e.header.destination, e.header.id,
+			e.header.created.tv_sec, e.header.created.tv_nsec, e.header.published.tv_sec, e.header.published.tv_nsec,
+			e.header.routed.tv_sec, e.header.routed.tv_nsec);
 	zmq_send(publisher, &e, size, 0);
 }
 
 struct event sub(size_t size) {
-	struct event e;
-	zmq_recv(subscriber, &e, size, 0);
-	return e;
+	char buf[size];
+	zmq_recv(subscriber, &buf, size, 0);
+	struct event *e = (struct event *) &buf;
+
+//	printf("%c;%c;%d;%lld.%09ld;%lld.%09ld;%lld.%09ld\n", e.header.source, e.header.destination, e.header.id,
+//			e.header.created.tv_sec, e.header.created.tv_nsec, e.header.published.tv_sec, e.header.published.tv_nsec,
+//			e.header.routed.tv_sec, e.header.routed.tv_nsec);
+	printf("%d\n", e);
+	return (*e);
+}
+
+void med(size_t size) {
+	zmq_msg_t msg_in;
+	assert(zmq_msg_init(&msg_in) == 0);
+	assert(zmq_msg_recv(&msg_in, subscriber, 0) == 0);
+	zmq_msg_t msg_out;
+	assert(zmq_msg_init_size(&msg_out, zmq_msg_size(&msg_in)) == 0);
+	memcpy(&msg_out, zmq_msg_data(&msg_in), zmq_msg_size(&msg_in));
+	assert(zmq_msg_send(&msg_out, publisher, 0) == 0);
 }
